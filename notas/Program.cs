6b4c373240c5a6c186cb6de25766b09463437a43
marinA <56300788+eternalSATA@users.noreply.github.com>
2019-10-11 13:45:05 +0200

    Add files via upload

---
+++ notas/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+
+namespace notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración variables
+            Console.WriteLine("SISTEMAS INFORMÁTICOS, UF 1");
+                Console.Write("Nota PAC 1: ");
+                    double s1_pac1 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 2: ");
+                    double s1_pac2 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 3: ");
+                    double s1_pac3 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 4: ");
+                    double s1_pac4 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 5: ");
+                    double s1_pac5 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 6: ");
+                    double s1_pac6 = double.Parse(Console.ReadLine());
+                Console.Write("Nota EXAMEN: ");
+                    double s_examen_1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("SISTEMAS INFORMÁTICOS, UF 2");
+                Console.Write("Nota PAC 1: ");
+                    double s2_pac1 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 2: ");
+                    double s2_pac2 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 3: ");
+                    double s2_pac3 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 4: ");
+                    double s2_pac4 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 5: ");
+                    double s2_pac5 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 6: ");
+                    double s2_pac6 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 7: ");
+                    double s2_pac7 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 8: ");
+                    double s2_pac8 = double.Parse(Console.ReadLine());
+                Console.Write("Nota EXAMEN: ");
+                    double s_examen_2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("SISTEMAS INFORMÁTICOS, UF 3");
+                Console.Write("Nota PAC 1: ");
+                    double s3_pac1 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 2: ");
+                    double s3_pac2 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 3: ");
+                    double s3_pac3 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 4: ");
+                    double s3_pac4 = double.Parse(Console.ReadLine());
+                Console.Write("Nota EXAMEN: ");
+                    double s_examen_3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("BASES DE DATOS, UF 1");
+                Console.Write("Nota PAC 1: ");
+                    double b1_pac1 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 2: ");
+                    double b1_pac2 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 3: ");
+                    double b1_pac3 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 4: ");
+                    double b1_pac4 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 5: ");
+                    double b1_pac5 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 6: ");
+                    double b1_pac6 = double.Parse(Console.ReadLine());
+                Console.Write("Nota EXAMEN: ");
+                    double b_examen_1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("BASES DE DATOS, UF 2");
+                Console.Write("Nota PAC 1: ");
+                    double b2_pac1 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 2: ");
+                    double b2_pac2 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 3: ");
+                    double b2_pac3 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 4: ");
+                    double b2_pac4 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 5: ");
+                    double b2_pac5 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 6: ");
+                    double b2_pac6 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 7: ");
+                    double b2_pac7 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 8: ");
+                    double b2_pac8 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 9: ");
+                    double b2_pac9 = double.Parse(Console.ReadLine());
+                Console.Write("Nota EXAMEN: ");
+                    double b_examen_2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("PROGRAMACIÓN, UF 1");
+                Console.Write("Nota PAC 1: ");
+                    double p1_pac1 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 2: ");
+                    double p1_pac2 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 3: ");
+                    double p1_pac3 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 4: ");
+                    double p1_pac4 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 5: ");
+                    double p1_pac5 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 6: ");
+                    double p1_pac6 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 7: ");
+                    double p1_pac7 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 8: ");
+                    double p1_pac8 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 9: ");
+                    double p1_pac9 = double.Parse(Console.ReadLine());
+                Console.Write("Nota EXAMEN: ");
+                    double p_examen_1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("PROGRAMACIÓN, UF 2");
+                Console.Write("Nota PAC 1: ");
+                    double p2_pac1 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 2: ");
+                    double p2_pac2 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 3: ");
+                    double p2_pac3 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 4: ");
+                    double p2_pac4 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 5: ");
+                    double p2_pac5 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 6: ");
+                    double p2_pac6 = double.Parse(Console.ReadLine());
+                Console.Write("Nota EXAMEN: ");
+                    double p_examen_2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("PROGRAMACIÓN, UF 3");
+                Console.Write("Nota PAC 1: ");
+                    double p3_pac1 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 2: ");
+                    double p3_pac2 = double.Parse(Console.ReadLine());
+                Console.Write("Nota PAC 3: ");
+                    double p3_pac3 = double.Parse(Console.ReadLine());
+                Console.Write("Nota EXAMEN: ");
+                    double p_examen_3 = double.Parse(Console.ReadLine());
+
+
+            // Cálculo medias
+            double s_media_1 = (0.4 * (0.8 * ((s1_pac1 + s1_pac2 + s1_pac3 + s1_pac4 + s1_pac5)/5.0) + 0.2 * s1_pac6)) + (0.6 * s_examen_1);
+            double s_media_2 = (0.4 * (0.5 * ((s2_pac1 + s2_pac2 + s2_pac3 + s2_pac4 + s2_pac5 + s2_pac6 + s2_pac7) / 7.0) + 0.5 * s2_pac8)) + (0.6 * s_examen_2);
+            double s_media_3 = (0.4 * (0.8 * ((s3_pac1 + s3_pac2 + s3_pac3) / 3.0) + 0.2 * s3_pac4)) + (0.6 * s_examen_3);
+            double s_media_total = (s_media_1 + s_media_2 + s_media_3) / 3;
+            double b_media_1 = (0.4 * (0.5 * ((b1_pac1 + b1_pac2 + b1_pac3 + b1_pac4 + b1_pac5) / 5.0) + 0.5 * b1_pac6)) + (0.6 * b_examen_1);
+            double b_media_2 = (0.4 * (0.8 * ((b2_pac1 + b2_pac2 + b2_pac3 + b2_pac4 + b2_pac5 + b2_pac6 + b2_pac7 + b2_pac8) / 8.0) + 0.2 * b2_pac9)) + (0.6 * b_examen_2);
+            double b_media_total = (b_media_1 + b_media_2) / 2;
+            double p_media_1 = (0.4 * (0.5 * ((p1_pac1 + p1_pac2 + p1_pac3 + p1_pac4 + p1_pac5 + p1_pac6 + p1_pac7 + p1_pac8) / 8.0) + 0.5 * p1_pac9)) + (0.6 * p_examen_1);
+            double p_media_2 = (0.4 * (0.5 * ((p2_pac1 + p2_pac2 + p2_pac3 + p2_pac4 + p2_pac5) / 5.0) + 0.5 * p2_pac6)) + (0.6 * p_examen_2);
+            double p_media_3 = (0.4 * (0.8 * ((p3_pac1 + p3_pac2) / 2.0) + 0.2 * p3_pac3)) + (0.6 * p_examen_3);
+            double p_media_total = (p_media_1 + p_media_2 + p_media_3) / 3;
+
+
+            // Notas finales
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("________________________________________________________________________________________________________________________________________________________________________");
+            Console.WriteLine("");
+
+            Console.WriteLine("SISTEMAS INFORMÁTICOS");
+            Console.WriteLine("UF 1:");
+            Console.WriteLine(s_media_1);
+            Console.WriteLine("UF 2:");
+            Console.WriteLine(s_media_2);
+            Console.WriteLine("UF 3:");
+            Console.WriteLine(s_media_3);
+            Console.WriteLine("");
+            Console.WriteLine("NOTA FINAL SISTEMAS INFORMÁTICOS:");
+            Console.WriteLine(s_media_total);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("BASES DE DATOS");
+            Console.WriteLine("UF 1:");
+            Console.WriteLine(b_media_1);
+            Console.WriteLine("UF 2:");
+            Console.WriteLine(b_media_2);
+            Console.WriteLine("");
+            Console.WriteLine("NOTA FINAL BASES DE DATOS:");
+            Console.WriteLine(b_media_total);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("PROGRAMACIÓN");
+            Console.WriteLine("UF 1:");
+            Console.WriteLine(p_media_1);
+            Console.WriteLine("UF 2:");
+            Console.WriteLine(p_media_2);
+            Console.WriteLine("UF 3:");
+            Console.WriteLine(p_media_3);
+            Console.WriteLine("");
+            Console.WriteLine("NOTA FINAL PROGRAMACIÓN:");
+            Console.WriteLine(p_media_total);
+        }
+    }
+}
